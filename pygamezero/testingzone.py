c2c6b30204459f06538b74042a7b01fcce33fadc
gfunkbus76 <60274564+gfunkbus76@users.noreply.github.com>
2021-02-05 13:59:19 -0800

    Update testingzone.py

--- pygamezero/testingzone.py
+++ pygamezero/testingzone.py
@@ -5,10 +5,14 @@ BACKGROUND_IMG = "textbackground"
 
 illuminate = Actor ("illuminationoff")
 illuminate.topleft = 42,627
+illuminate.status = False
 
 def draw():
     screen.blit(BACKGROUND_IMG, (0,0))
     illuminate.draw()
+    
+def update():
+    pass
     
 def set_illumination_on():
     illuminate.image = "illuminationon"
@@ -16,10 +20,14 @@ def set_illumination_off():
     illuminate.image = "illuminationoff"
 
 
-def on_mouse_down(pos):
-    if illuminate.collidepoint(pos):
-        set_illumination_on()
-        
+def on_mouse_down(pos, button):
+    if button == mouse.LEFT:
+        if illuminate.collidepoint(pos):
+            illuminate.status = True
+            set_illumination_on()
+        else:
+            illuminate.status = False
+            set_illumination_off
         
 #def update():
 
