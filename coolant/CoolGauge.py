728fa31710689f73b31078a6e2d3ff21f3344f92
gfunkbus76 <60274564+gfunkbus76@users.noreply.github.com>
2021-03-01 13:47:34 -0800

    Organizing more, integrating some items from miata-dash project on github, trying to move thigns into more class structure. Bear with me. Also, tryiun to start a 'dev' branch, so I can do that while keeping something workable. But I probably wont succeed ... as previously mentioned work in progress!

---
+++ coolant/CoolGauge.py
@@ -0,0 +1,42 @@
+import pygame
+import os 
+from pygame.locals import *
+# THIS IS FOR A COOLANT GAUGE
+
+#Coolant Gauge Class
+class CoolantGauge:
+	def __init__(self, posx, posy):
+		self.posx = posx
+		self.posy = posy
+		self.image = 0
+		self.frame = 0
+		self.set_image(self.frame)
+		self.grw_flag = True
+
+	def get_image(self):
+		return self.image
+
+	def set_image(self, frame):
+		image = str(frame)+".png"
+		self.image = pygame.image.load('coolant/' + image)
+
+	def get_pos(self):
+		return (self.posx, self.posy)
+
+	def set_frame(self, frame):
+		self.frame = frame
+		self.set_image(frame)
+
+	def get_frame(self):
+		return self.frame
+
+	def show(self, screen):
+		screen.blit(self.get_image(), self.get_pos())
+		if self.get_frame() == 15:
+			self.grw_flag = False
+		if self.get_frame() == 0:
+			self.grw_flag = True
+		if self.grw_flag:
+			self.set_frame(self.get_frame() + 1)
+		else:
+			self.set_frame(self.get_frame() - 1)
